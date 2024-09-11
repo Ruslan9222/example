c5f03ffaf05844006f756280d6d1c9175b576fd0
Ruslan9222 <magog21@gmail.com>
2024-09-11 22:27:13 +0300

    switch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import by.ruslan.radzevich.cycle.SwitchCase;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        SwitchCase.switchCase();
     }
 }
