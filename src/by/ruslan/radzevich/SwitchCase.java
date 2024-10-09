0c7d01f5b95d8e60e831783cf60889243cee722c
Ruslan9222 <magog21@gmail.com>
2024-10-09 15:24:44 +0300

    Tag 0.1

--- src/by/ruslan/radzevich/SwitchCase.java
+++ src/by/ruslan/radzevich/SwitchCase.java
@@ -15,7 +15,9 @@ public class SwitchCase {
            case 5 -> System.out.println("5");
            default -> System.out.println("default");
        }
+        System.out.println("1");
     }
+
 
 
 
