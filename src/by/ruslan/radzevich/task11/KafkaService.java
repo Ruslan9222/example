fea0dc75822047e3db358930c9b588820e26c810
Ruslan9222 <magog21@gmail.com>
2025-07-15 17:28:25 +0300

    add tasks

--- src/by/ruslan/radzevich/task11/KafkaService.java
+++ src/by/ruslan/radzevich/task11/KafkaService.java
@@ -31,3 +31,47 @@
 
 //    @Mapping(target = "addressId", source = "address.addressId")
 //    NewInsuranceMessageDto toNewInsuranceMessageDto(PropertyInsuranceDto insurance);
+
+//@ExtendWith(MockitoExtension.class)
+//class KafkaServiceImplTest {
+//
+//    @Mock
+//    private KafkaSender sender;
+//
+//    @Mock
+//    private KafkaProperties properties;
+//
+//    @Mock
+//    private KafkaProperties.Producer producer;
+//
+//    private KafkaServiceImpl kafkaService;
+//
+//    @BeforeEach
+//    void setup() {
+//        kafkaService = new KafkaServiceImpl(sender, properties);
+//    }
+//
+//    @Test
+//    void send_ShouldSendMessageToCorrectTopic() {
+//        // Arrange
+//        NewInsuranceMessageDto message = new NewInsuranceMessageDto(
+//                UUID.randomUUID(),
+//                UUID.randomUUID(),
+//                InsuranceStatus.APPROVED,
+//                LocalDate.of(2025, 1, 1),
+//                LocalDate.of(2026, 1, 1),
+//                PaymentFrequency.ONCE_IN_MONTH,
+//                new BigDecimal("5000.00"),
+//                new BigDecimal("416.67")
+//        );
+//
+//        when(properties.getProducer()).thenReturn(producer);
+//        when(producer.getTopicNames()).thenReturn(
+//                Map.of("property-insurance-created", "test-topic"));
+//
+//        // Act
+//        kafkaService.send(message);
+//
+//        // Assert
+//        verify(sender).sendAsync("test-topic", message);
+//    }
