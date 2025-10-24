24d5ee0ff64a40975505ff0c949fd69e1ce1c4bd
Ruslan9222 <magog21@gmail.com>
2025-10-24 17:19:48 +0300

    add tasks

--- exampleFromBook/src/main/java/by/ruslan/radzevich/exercisesFromChapterTwo/task1/AlgoritmicTask.java
+++ exampleFromBook/src/main/java/by/ruslan/radzevich/exercisesFromChapterTwo/task1/AlgoritmicTask.java
@@ -3,5 +3,6 @@ package by.ruslan.radzevich.exercisesFromChapterTwo.task1;
 public interface AlgoritmicTask {
 
     void findAndPrintShortestAndLongest(char[] chars);
+
     void maxAndMinNumbersForLength();
 }
--- exampleFromBook/src/main/java/by/ruslan/radzevich/exercisesFromChapterTwo/task1/AlgoritmicTaskImpl.java
+++ exampleFromBook/src/main/java/by/ruslan/radzevich/exercisesFromChapterTwo/task1/AlgoritmicTaskImpl.java
@@ -7,6 +7,7 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.OptionalDouble;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
